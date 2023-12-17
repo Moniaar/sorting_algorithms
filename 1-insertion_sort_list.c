3c23d2d3139e4bd4b27514c4a714f14f5783b363
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2023-12-17 18:51:25 +0000

    Go

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * swap_liselm - swaps 2 nodes in a doubly linked list
+ *
+ * @m: address of the first node
+ *
+ * @n: address of the seconed node
+ *
+ * Return: Nothing (Void)
+ */
+
+void swap_liselm(listint_t *m, listint_t *n)
+{
+
+void insertion_sort_list(listint_t **list)
+{
+
