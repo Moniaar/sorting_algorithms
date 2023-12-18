5d9cdb987c16fcc6d8e5b901d07cfd48823f5c14
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-18 04:07:55 -0800

    mini debugging session started right now

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,28 +1,75 @@
 #include <stdio.h>
 #include "sort.h"
-void swap(int* a, int* b) {
+
+/**
+ * swap_elem - a function to swap 2 elements from
+ * the array
+ *
+ * @a: the first element that needs to be swapped
+ *
+ * @b: the seconed element that needs to be swapped
+ *
+ * Return: Nothing (void)
+ */
+
+void swap_elem(int *a, int *b)
+{
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-int partition(int arr[], int low, int high) {
+/**
+ * partition - a function to do Lomuto partition for the
+ * start of the sorting section
+ *
+ * @arr: the input array that needs to be sorted
+ *
+ * @low: the lowest element in the array
+ *
+ * @high: the highest element inside the array
+ *
+ * Return: integer postion of the current pivot
+ */
+
+int partition(int arr[], int low, int high)
+{
 	int pivot = arr[high];
 	int i = (low - 1);
+	int j;
 
-	for (int j = low; j <= high - 1; j++) {
-		if (arr[j] < pivot) {
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
 			i++;
-			swap(&arr[i], &arr[j]);
+			swap_elem(&arr[i], &arr[j]);
 		}
 	}
-	swap(&arr[i + 1], &arr[high]);
+	swap_elem(&arr[i + 1], &arr[high]);
 	return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high) {
-	if (low < high) {
-		int pi = partition(arr, low, high);
+/**
+ * quickSort - a function to implement the algorithm of quicksort
+ *
+ * @arr: the given array element as an input to be sorted
+ *
+ * @low: the lowest element in the array
+ *
+ * @high: the highest element inside the array
+ *
+ * Return: integer postion of the current pivot
+ */
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi;
+
+		pi = partition(arr, low, high);
+
 		quickSort(arr, low, pi - 1);
 		quickSort(arr, pi + 1, high);
 	}
