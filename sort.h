5aa55c04c45003c5d30f1ab42fd097353e225781
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-17 22:29:22 -0800

    1. Insertion sort

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+void swap_liselm(listint_t *m, listint_t *n);
 void insertion_sort_list(listint_t **list);
 
 #endif
