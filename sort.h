53feaa136c03edd52e9d209ae8d679784c094f52
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-18 02:13:06 -0800

    adding selection sort to the header file

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void swap_liselm(listint_t *m, listint_t *n);
+void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
 #endif
